e7e267976efab61aba0ff6320bc68129d610e6dd
Edward Betts <edward@4angle.com>
2025-02-19 00:20:53 +0000

    Update homepage URL

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python library for the IoTaWatt Energy device",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gtdiehl/iotawattpy",
+    url="https://github.com/home-assistant-libs/iotawattpy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
